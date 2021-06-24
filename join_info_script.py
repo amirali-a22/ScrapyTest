30d430178147622c4325fdb78e55c9be5873ddc8
amirali <amirali.arn.a22@gmail.com>
2021-06-24 23:42:25 +0430

    make best practice

--- join_info_script.py
+++ join_info_script.py
@@ -1,113 +1,73 @@
 import json
-import re
 
-drug_list = []
-with open('scrapyTest/drugs_info.json', 'r', encoding='utf-8') as file:
-    for drug in file:
-        test = drug.split('\n')
-        drug_list.append(test[0][:-1])
+with open('scrapyTest/drugs_info.json', 'r', encoding='utf-8') as drugs_file:
+    drugs_file = json.loads(drugs_file.read())
 
-for i, link in enumerate(drug_list):
-    # print(link)
+# print(type(drugs_file[0]))
+
+with open('insurances/tamin.json', 'r', encoding='utf-8') as tamin_file:
+    tamin_file = json.loads(tamin_file.read())
+
+# print(tamin_file[0]['GeneralCode'])
+# print(tamin_file[0]['MinPrice'])
+# print(tamin_file[0]['MaxPrice'])
+
+
+with open('insurances/salamat.json', 'r', encoding='utf-8') as salamat_file:
+    salamat_file = json.loads(salamat_file.read())
+
+# print(salamat_file[0]['GeneralCode'])
+# print(salamat_file[0]['MinPrice'])
+# print(salamat_file[0]['MaxPrice'])
+
+with open('insurances/niro_mosalah.json', 'r', encoding='utf-8') as niro_mosalah_file:
+    niro_mosalah_file = json.loads(niro_mosalah_file.read())
+
+# print(niro_mosalah_[0]['GeneralCode'])
+# print(niro_mosalah_[0]['MinPrice'])
+# print(niro_mosalah_[0]['MaxPrice'])
+
+for i, drug in enumerate(drugs_file):
     try:
-        drug_list[i] = json.loads(link)
-    except:
-        pass
-drug_list.pop(0)
-drug_list.pop(len(drug_list) - 1)
+        temp = drugs_file[i]['price']
+        drugs_file[i]['price'] = []
 
-tamin_data_list = []
-with open('insurances/tamin.json', 'r', encoding='utf-8') as file:
-    file = file.read()[1:-1].replace('},', '},\n').split('\n')
-    for drug in file:
-        try:
-            tamin_data_list.append(json.loads(drug[:-1]))
-        except:
-            pass
-
-# print(tamin_data_list[0]['GeneralCode'])
-# print(tamin_data_list[0]['MinPrice'])
-# print(tamin_data_list[0]['MaxPrice'])
-
-salamat_data_list = []
-with open('insurances/salamat.json', 'r', encoding='utf-8') as file:
-    file = file.read()[1:-1].replace('},', '},\n').split('\n')
-    for drug in file:
-        try:
-            salamat_data_list.append(json.loads(drug[:-1]))
-        except:
-            pass
-
-# print(salamat_data_list[0]['GeneralCode'])
-# print(salamat_data_list[0]['MinPrice'])
-# print(salamat_data_list[0]['MaxPrice'])
-
-niro_mosalah_data_list = []
-with open('insurances/niro_mosalah.json', 'r', encoding='utf-8') as file:
-    file = file.read()[1:-1].replace('},', '},\n').split('\n')
-    for drug in file:
-        try:
-            niro_mosalah_data_list.append(json.loads(drug[:-1]))
-        except:
-            pass
-
-# print(niro_mosalah_data_list[0]['GeneralCode'])
-# print(niro_mosalah_data_list[0]['MinPrice'])
-# print(niro_mosalah_data_list[0]['MaxPrice'])
-
-for i, drug in enumerate(drug_list):
-    try:
-        temp = drug_list[i]['price']
-        drug_list[i]['price'] = []
-        # print(tamin_data_list[i]['GeneralCode'])
-        # print(drug_list[i]['generic_code'])
-
-        for j, tamin_drug in enumerate(tamin_data_list):
-            if tamin_data_list[j]['GeneralCode'] == drug_list[i]['generic_code']:
-                # print(tamin_data_list[i]['MinPrice'])
+        for j, tamin_drug in enumerate(tamin_file):
+            if tamin_file[j]['GeneralCode'] == drugs_file[i]['generic_code']:
+                # print(tamin_file[i]['MinPrice'])
                 temp_dict = {
                     'insurance': 'tamin',
-                    'MinPrice': tamin_data_list[i]['MinPrice'],
-                    'MaxPrice': tamin_data_list[i]['MaxPrice']
+                    'MinPrice': tamin_file[i]['MinPrice'],
+                    'MaxPrice': tamin_file[i]['MaxPrice']
                 }
                 # temp_dict = {'test': 'test'}
-                drug_list[i]['price'].append(temp_dict)
-
-        for z, salamat_drug in enumerate(salamat_data_list):
-            if salamat_data_list[z]['GeneralCode'] == drug_list[i]['generic_code']:
-                temp_dict = [{
+                drugs_file[i]['price'].append(temp_dict)
+        for z, salamat_drug in enumerate(salamat_file):
+            if salamat_file[z]['GeneralCode'] == drugs_file[i]['generic_code']:
+                temp_dict = {
                     'insurance': 'salamat',
-                    'MinPrice': salamat_data_list[i]['MinPrice'],
-                    'MaxPrice': salamat_data_list[i]['MaxPrice']
-                }]
-                drug_list[i]['price'].append(temp_dict)
+                    'MinPrice': salamat_file[i]['MinPrice'],
+                    'MaxPrice': salamat_file[i]['MaxPrice']
+                }
+                drugs_file[i]['price'].append(temp_dict)
 
-        for y, niro_mosalah_drug in enumerate(niro_mosalah_data_list):
-            if niro_mosalah_data_list[y]['GeneralCode'] == drug_list[i]['generic_code']:
+        for y, niro_mosalah_drug in enumerate(niro_mosalah_file):
+            if niro_mosalah_file[y]['GeneralCode'] == drugs_file[i]['generic_code']:
                 temp_dict = {
                     'insurance': 'niro_mosalah',
-                    'MinPrice': salamat_data_list[i]['MinPrice'],
-                    'MaxPrice': salamat_data_list[i]['MaxPrice']
+                    'MinPrice': niro_mosalah_file[i]['MinPrice'],
+                    'MaxPrice': niro_mosalah_file[i]['MaxPrice']
                 }
-                drug_list[i]['price'].append(temp_dict)
+                drugs_file[i]['price'].append(temp_dict)
 
-        if len(drug_list[i]['price']) == 0:
-            drug_list[i]['price'].append(int(temp))
-        # print(drug_list[i]['price'])
-        # print(drug_list[i]['generic_code'])
+        if len(drugs_file[i]['price']) == 0:
+            drugs_file[i]['price'].append(int(temp))
+        # print(drugs_file[i]['price'])
+        # print(drugs_file[i]['generic_code'])
     except:
         pass
 
-with open('drug_complete_data.json', 'a') as file:
-    file.write('[')
-for n, drug in enumerate(drug_list):
-    # try:
-    #     with open('drug_complete_data.json') as file:
-    #         file.write(drug_list[n])
-    with open('drug_complete_data.json', 'a') as file:
-        file.write(str(drug_list[n]))
-    # except:
-    #     pass
+# print(drugs_file[0])
+with open('drug_complete_data.json', 'w', encoding='utf-8') as file:
+    file.write(json.dumps(drugs_file))
 
-with open('drug_complete_data.json', 'a') as file:
-    file.write(']')
